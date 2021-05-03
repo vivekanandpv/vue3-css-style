e73488a70f28b2f3415442fbde25192178beb164
vivek <vivek@athenaeum.in>
2021-05-03 17:36:43 +0530

    done

--- Example.vue
+++ Example.vue
@@ -1,13 +1,27 @@
 <template>
-  <p>Learning CSS and style</p>
+  <div>
+    <p :class="{ 'red-text': enableRed }">Learning CSS and style: Red?</p>
+    <p :class="{ 'blue-text': enableBlue }">Learning CSS and style: Blue?</p>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data: function () {
+    return {
+      enableRed: true,
+      enableBlue: false,
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.red-text {
+  color: red;
+}
 
+.blue-text {
+  color: blue;
+}
 </style>
